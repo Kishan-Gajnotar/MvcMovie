b198f9b87b023229f1dba2cdbe2bb0bdc855e8e6
HP <HP@DESKTOP-EL0669V>
2023-12-03 15:05:27 +0530

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+        public IActionResult Welcome(string name = "kishan", int numTimes = 1) // HelloWorld/Welcome?name=Rick&numtimes=4
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
